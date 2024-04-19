e2246068833f7a97f26cec7a7c2cc4eb16c8ed26
AqibShaheer <65130459+AqibShaheer@users.noreply.github.com>
2024-04-19 13:42:38 -0700

    this is a number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "guessanumber",
+        type: "number",
+        message: "please enter a number",
+    },
+]);
+if (answers.guessanumber == randomnumber) {
+    console.log("congratulations you guessed a right number");
+}
+else {
+    console.log("wrong guess");
+}
